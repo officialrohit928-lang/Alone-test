f5a1cb5ce80a281f1c1a25f2d087d35b382e864c
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-18 12:06:02 +0530

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -1,54 +1,42 @@
-# Copyright (C) 2021-2022 by TheAloneteam@Github
-# PyroFork compatible button panel
+#
+# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
+#
+# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
+#
+# All rights reserved.
 
 from pyrogram.types import InlineKeyboardButton
+
 import config
 from AloneMusic import app
 
-# Start panel (public)
+
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(
-                text=_["S_B_2"],
-                url=config.SUPPORT_CHAT
-            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
 
-# Private panel (DM / PM)
+
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-                color="green"  # 🟢 Green for Add Me
             )
         ],
         [
-            InlineKeyboardButton(
-                text=_["S_B_2"],
-                callback_data="shiv_aarumi",
-                color="blue"  # 🔵 Blue for Support
-            ),
-            InlineKeyboardButton(
-                text="💌 ʏᴛ-ᴀᴘɪ",
-                callback_data="bot_info_data",
-                color="gray"  # ⚪ Grey
-            ),
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="shiv_aarumi"),
+            InlineKeyboardButton(text="💌 ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                callback_data="settings_back_helper",
-                color="red"  # 🔴 Red for Back
-            )
-        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
