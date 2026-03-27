0ff5cdfbad722e0798f9b05da26bd8ecfa517a13
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-27 06:58:45 +0530

    Update start.py

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -47,7 +47,7 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="settings_back_helper",
-                style=ButtonStyle.SECONDARY
+                style=ButtonStyle.DANGER
             )
         ],
     ]
