b573370c1a8c2d1c67fefc817b6f9778570f6da9
officialrohit928-lang <officialrohit928@gmail.com>
2026-03-27 07:18:50 +0530

    Change button style from SUCCESS to SECONDARY

--- AloneMusic/utils/inline/start.py
+++ AloneMusic/utils/inline/start.py
@@ -15,7 +15,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_CHAT,
-                style=ButtonStyle.SUCCESS
+                style=ButtonStyle.SECONDARY
             ),
         ],
     ]
